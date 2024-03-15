17c16124f067ac47f44b6a3eca29fd6e484cac3e
mahiboobnadaf <rasool97260@gmail.com>
2024-03-15 18:39:25 +0530

    Continous Counter in React

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  let [count,setCount] = useState(0)
+
+  let [timerId,setTimerId] = useState("")
+  
+  let start=()=>{
+    let id = setInterval(increment,1000)
+    setTimerId(id)
+  }
+  
+  let increment=()=>{
+    setCount(count=>count+1)
+  }
+  
+  let stop=()=>{
+    clearInterval(timerId)
+  }
+  return (
+    <>
+      <h1>Counter : {count}</h1>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+        
+    </>
+  )
+}
+
+export default App
